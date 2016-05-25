5548638db43beb9f8ef4afa9bae1721e1c47e9cf
DejanUrosevic <dejansuca@gmail.com>
2016-05-25 23:28:48 +0200

    Zaboravljen xml fajl u kojem se nalaze propisi

---
+++ HelloWeb01/HelloWeb/src/web/xml/model/Propisi.java
@@ -0,0 +1,29 @@
+package web.xml.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import jaxb.from.xsd.Propis;
+
+@XmlRootElement(name = "propisi")
+public class Propisi {
+	private List<Propis> propisi = new ArrayList<Propis>();
+
+	public Propisi() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@XmlElement(name = "propis")
+	public List<Propis> getPropisi() {
+		return propisi;
+	}
+
+	public void setPropisi(List<Propis> propisi) {
+		this.propisi = propisi;
+	}
+	
+	
+}
